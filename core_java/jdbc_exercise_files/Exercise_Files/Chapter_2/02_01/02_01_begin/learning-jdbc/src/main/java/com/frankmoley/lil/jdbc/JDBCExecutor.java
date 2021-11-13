f88d33c33e6e9c02b6d20dd8504456688431c1b7
Josh Wessel <joshwessel9@gmail.com>
2021-11-13 17:22:54 +0000

    Deleted misplaced copy of exercise files directory

--- core_java/jdbc_exercise_files/Exercise_Files/Chapter_2/02_01/02_01_begin/learning-jdbc/src/main/java/com/frankmoley/lil/jdbc/JDBCExecutor.java
+++
@@ -1,8 +0,0 @@
-package com.frankmoley.lil.jdbc;
-
-public class JDBCExecutor {
-
-    public static void main(String... args){
-        System.out.println("Hello Learning JDBC");
-    }
-}
--- core_java/jdbc_exercise_files/Exercise_Files/Chapter_2/02_10/02_10_end/learning-jdbc/src/main/java/com/frankmoley/lil/jdbc/util/DataTransferObject.java
+++
@@ -1,6 +0,0 @@
-package com.frankmoley.lil.jdbc.util;
-
-public interface DataTransferObject {
-
-    long getId();
-}

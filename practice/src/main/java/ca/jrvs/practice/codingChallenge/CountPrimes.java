bc0ed136b4e624253ef048fc64728bb3cfa0e9e4
Josh Wessel <joshwessel9@gmail.com>
2021-11-19 02:47:43 +0000

    Coding Challenge: CountPrimes (Updated formatting)

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/CountPrimes.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/CountPrimes.java
@@ -12,18 +12,21 @@ public class CountPrimes {
     public int countPrimes(int n) {
       int count = 0;
       for (int i = 0; i < n; i++) {
-        if (isPrime(i))
+        if (isPrime(i)) {
           count++;
+        }
       }
       return count;
     }
 
     public boolean isPrime(int n) {
-      if (n <= 1)
+      if (n <= 1) {
         return false;
+      }
       for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
+        if (n % i == 0) {
           return false;
+        }
       }
       return true;
     }
